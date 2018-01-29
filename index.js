7c41f56198acea49b94654f92de380d41f222b9b
Blake Kunishige <blake3457@yahoo.com>
2018-01-29 01:53:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,8 @@ function add2(n) {
   const two = 2
 }
 
-var funkyFunction = function() {
-  return function() {
+function funkyFunction() {
     return "FUNKY!"
-  }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
